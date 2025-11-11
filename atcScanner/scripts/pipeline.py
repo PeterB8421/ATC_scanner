56e52f878b2ca92a3e2466c754001721f6c59c29
Petr Balok <petrbalok@gmail.com>
2025-11-11 16:01:17 +0100

    Finished pipeline.py to use config and signal handling, added logger

--- atcScanner/scripts/pipeline.py
+++ atcScanner/scripts/pipeline.py
@@ -3,6 +3,8 @@ import glob
 import subprocess
 import time
 import signal
+import json
+import logging
 
 
 # Gets year from filename
@@ -24,22 +26,29 @@ def get_day(filename):
 
 
 # Processes a new raw file
-def process_file(input_file):
+def process_file(input_file, file_ext):
     # Change output file name to wav audio file
-    output_filename = input_file.split('/')[-1].replace('.cs16', '.wav')
+    output_filename = input_file.split('/')[-1].replace(file_ext, '.wav')
     # Creates file path according to date of the file (taken from filename)
     output_filepath = '../audio_files/' + get_year(output_filename) + '/' + get_month(output_filename) + '/' + get_day(output_filename) + '/'
     os.makedirs(output_filepath, exist_ok=True)
 
     try:
+        logging.info('Processing file ' + output_filename.replace('.wav', file_ext))
         subprocess.run(['./decode_cs16.sh', input_file, output_filepath + output_filename], check=True)
     except subprocess.CalledProcessError as e:
         # Prints an error if processing fails
-        print('Processing failed!')
-        print(e)
+        logging.error('Failed to decode file ' + output_filename.replace('.wav', file_ext))
         return
     # Delete raw file
     os.remove(input_file)
+
+
+def get_config():
+    config_file = 'conf/pipeline.json'
+    with open(config_file, 'r') as f:
+        settings = json.load(f)
+    return settings
 
 
 class GracefulShutdown:
@@ -55,21 +64,31 @@ class GracefulShutdown:
 
 
 def main():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logging.info('Pipeline started')
     # Path with raw transmission files
     raw_files_path = '../records/'
     # Check if the path exists
     if not os.path.exists(raw_files_path):
-        print('Path for raw files not found')
+        logging.critical('Raw files path does not exist!')
+
+    settings = get_config()
+    sleep_time = settings['sleep_time']
+    file_ext = settings['file_ext']
 
     terminator = GracefulShutdown()
 
     # Periodically check if there are any new files, exit if requested
     while not terminator.exit_needed:
-        raw_files = glob.glob(raw_files_path + '*.cs16')
-        print(raw_files)
+        raw_files = glob.glob(raw_files_path + '*' + file_ext)
         for f in raw_files:
-            process_file(f)
-        time.sleep(5)
+            process_file(f, file_ext)
+        time.sleep(sleep_time)
+    logging.info('Pipeline terminated')
 
 
 if __name__ == '__main__':
