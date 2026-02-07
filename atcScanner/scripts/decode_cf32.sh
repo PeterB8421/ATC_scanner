b15d1b541d50686c7b38c24918ae3fbdcfd8b968
Petr Balok <petrbalok@gmail.com>
2026-02-07 22:53:18 +0100

    Fixed cf32 file conversion

--- atcScanner/scripts/decode_cf32.sh
+++ atcScanner/scripts/decode_cf32.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cf32=$1
 file_out=$2
-cat $cf32 | csdr amdemod_cf | csdr dcblock_ff | sox -t raw -r8k -c1 -b32 -e float - -t wav $file_out
+cat $cf32 | csdr amdemod_cf | csdr dcblock_ff | csdr fastagc_ff | sox -t raw -r 8000 -e float -b 32 -c 1 - "$file_out" highpass 300 lowpass 3400 vad -t 5 -p 0.5
